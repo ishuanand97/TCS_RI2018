16c71c71ad6f43792eb0143c92d2ab88ca19b59d
ishuanand97 <you@example.com>
2018-07-31 14:20:59 +0530

    added severity levels

--- app.js
+++ app.js
@@ -15,10 +15,33 @@ function myFunction() {
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             var javaObj = JSON.parse(xhttp.response);
-            var newval=parseFloat(javaObj[x]).toFixed(2);
+            var newval = parseFloat(javaObj[x]).toFixed(2);
             document.getElementById("demo").innerHTML = newval;
-
-            var dVal = newval/5;
+            s = newval;
+            var z;
+            if (s != NaN) {
+                if (s > 0 && s < 50) {
+                    z = "Good";
+                    $('#severity').css("color", "#009966");
+                } else if (s > 51 && s < 100) {
+                    z = "Satisfactory";
+                    $('#severity').css("color", "#ffde33");
+                } else if (s > 101 & s < 200) {
+                    z = "Moderately polluted";
+                    $('#severity').css("color", "#ff9933");
+                } else if (s > 201 && s < 300) {
+                    z = "Poor";
+                    $('#severity').css("color", "#cc0033");
+                } else if (s > 301 && s < 400) {
+                    z = "Very poor";
+                    $('#severity').css("color", "#660099");
+                } else {
+                    z = "Severe";
+                    $('#severity').css("color", "#7e0023");
+                }
+                document.getElementById("severity").innerHTML = z;
+            }
+            var dVal = newval / 5;
             var newVal = dVal * 1.8 - 45;
             $('.circle-inner, .gauge-copy').css({
                 'transform': 'rotate(' + newVal + 'deg)'
@@ -28,7 +51,7 @@ function myFunction() {
             });
             $('.percentage').text(dVal.toFixed(2) + ' %');
 
-            if (dVal >20) {
+            if (dVal > 20) {
                 if (document.querySelector('#clouds').children[0].classList.contains("cloud-w")) {
                     var len = document.querySelector('#clouds').children.length;
                     for (var i = 0; i < len; i++) {
